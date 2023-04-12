65e22a5c35c51a01c031cb0ef980a1feb3e78523
jounikivi <jounikivipera@gmail.com>
2023-04-12 09:38:25 +0300

    aloitin uudestaan

--- pistetaulukko/src/Pelaaja.java
+++ pistetaulukko/src/Pelaaja.java
@@ -1,25 +1,40 @@
+/**
+ * Pelaaja-luokka tallentaa pelaajan nimen ja pistemäärän.
+ */
 public class Pelaaja {
-  private String nimi;
-  private int pisteet;
-  
-  // Konstruktori, joka ottaa vastaan pelaajan nimen ja alustaa pelaajan pistemäärän nollaksi
-  public Pelaaja(String nimi) {
-      this.nimi = nimi;
-      this.pisteet = 0;
-  }
-  
-  // Palauttaa pelaajan nimen
-  public String getNimi() {
-      return nimi;
-  }
-  
-  // Palauttaa pelaajan pistemäärän
-  public int getPisteet() {
-      return pisteet;
-  }
-  
-  // Asettaa pelaajan pistemäärän
-  public void setPisteet(int pisteet) {
-      this.pisteet = pisteet;
-  }
+    private String nimi;
+    private int pisteet;
+
+    /**
+     * Konstruktori luo uuden pelaajan, jolla on annettu nimi ja 0 pistettä.
+     * @param nimi Pelaajan nimi.
+     */
+    public Pelaaja(String nimi) {
+        this.nimi = nimi;
+        this.pisteet = 0;
+    }
+
+    /**
+     * Palauttaa pelaajan nimen.
+     * @return Pelaajan nimi.
+     */
+    public String getNimi() {
+        return nimi;
+    }
+
+    /**
+     * Palauttaa pelaajan pistemäärän.
+     * @return Pelaajan pistemäärä.
+     */
+    public int getPisteet() {
+        return pisteet;
+    }
+
+    /**
+     * Lisää annetun pistemäärän pelaajan kokonaispistemäärään.
+     * @param pistemaara Lisättävä pistemäärä.
+     */
+    public void lisaaPisteet(int pistemaara) {
+        pisteet += pistemaara;
+    }
 }
