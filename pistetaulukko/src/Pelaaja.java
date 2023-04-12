44e81fdab4c24eab0582ac59177d825eb313835d
jounikivi <jounikivipera@gmail.com>
2023-04-12 11:16:50 +0300

    muutoksia

--- pistetaulukko/src/Pelaaja.java
+++ pistetaulukko/src/Pelaaja.java
@@ -1,40 +1,30 @@
-/**
- * Pelaaja-luokka tallentaa pelaajan nimen ja pistemäärän.
- */
 public class Pelaaja {
     private String nimi;
     private int pisteet;
 
-    /**
-     * Konstruktori luo uuden pelaajan, jolla on annettu nimi ja 0 pistettä.
-     * @param nimi Pelaajan nimi.
-     */
-    public Pelaaja(String nimi) {
+    // Konstruktori, joka luo uuden pelaajan annetulla nimellä ja aloituspisteillä
+    public Pelaaja(String nimi, int pisteet) {
         this.nimi = nimi;
-        this.pisteet = 0;
+        this.pisteet = pisteet;
     }
 
-    /**
-     * Palauttaa pelaajan nimen.
-     * @return Pelaajan nimi.
-     */
+    // Getteri pelaajan nimelle
     public String getNimi() {
         return nimi;
     }
 
-    /**
-     * Palauttaa pelaajan pistemäärän.
-     * @return Pelaajan pistemäärä.
-     */
+    // Setteri pelaajan nimelle
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
+
+    // Getteri pelaajan pisteille
     public int getPisteet() {
         return pisteet;
     }
 
-    /**
-     * Lisää annetun pistemäärän pelaajan kokonaispistemäärään.
-     * @param pistemaara Lisättävä pistemäärä.
-     */
-    public void lisaaPisteet(int pistemaara) {
-        pisteet += pistemaara;
+    // Setteri pelaajan pisteille
+    public void setPisteet(int pisteet) {
+        this.pisteet = pisteet;
     }
 }
--- pistetaulukko/src/Pelisessio.java
+++ pistetaulukko/src/Pelisessio.java
@@ -1,3 +1,58 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Pelisessio {
-    
+    private Date aloitusPvm;
+    private Date lopetusPvm;
+    private ArrayList<Integer> heitot;
+
+    /**
+     * Konstruktori, joka luo uuden Pelisessio-olion
+     * ja asettaa aloitusPvm:n nykyhetkeksi.
+     */
+    public Pelisessio() {
+        aloitusPvm = new Date();
+        heitot = new ArrayList<Integer>();
+    }
+
+    /**
+     * Palauttaa pelisession aloituspäivämäärän.
+     * @return aloitusPvm aloituspäivämäärä
+     */
+    public Date getAloitusPvm() {
+        return aloitusPvm;
+    }
+
+    /**
+     * Asettaa pelisession lopetuspäivämäärän.
+     * @param lopetusPvm lopetuspäivämäärä
+     */
+    public void setLopetusPvm(Date lopetusPvm) {
+        this.lopetusPvm = lopetusPvm;
+    }
+
+    /**
+     * Palauttaa pelisession lopetuspäivämäärän.
+     * @return lopetusPvm lopetuspäivämäärä
+     */
+    public Date getLopetusPvm() {
+        return lopetusPvm;
+    }
+
+    /**
+     * Lisää uuden heiton pelisessioon.
+     * @param tulos heiton tulos
+     */
+    public void lisaaHeitto(int tulos) {
+        heitot.add(tulos);
+    }
+
+    /**
+     * Palauttaa kaikki pelisession heitot.
+     * @return heitot pelisession heitot
+     */
+    public ArrayList<Integer> getHeitot() {
+        return heitot;
+    }
 }
+
